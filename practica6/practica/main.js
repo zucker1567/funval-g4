7badd03c0be0b715b12631dd5cdd51661d6f221b
zucker1567 <johan.romo.2000@gmail.com>
2025-07-18 10:07:14 -0400

    practica 6 y tarea no terminada

--- practica6/practica/main.js
+++ practica6/practica/main.js
@@ -2,44 +2,44 @@ let saldo = 0;
 let opcion;
 
 do {
-  opcion = parseInt(prompt(
+    opcion = parseInt(prompt(
     "Elige una opción:\n1. Ingresar dinero\n2. Retirar dinero\n3. Consultar saldo\n4. Salir"
-  ));
+    ));
 
-  switch (opcion) {
-    case 1:
-      let ingreso = parseFloat(prompt("¿Cuánto deseas ingresar?"));
-      if (!isNaN(ingreso) && ingreso > 0) {
-        saldo += ingreso;
-        alert("Ingreso exitoso. Nuevo saldo: $" + saldo);
-      } else {
-        alert("Cantidad inválida.");
-      }
-      break;
-
-    case 2:
-      let retiro = parseFloat(prompt("¿Cuánto deseas retirar?"));
-      if (!isNaN(retiro) && retiro > 0) {
-        if (retiro <= saldo) {
-          saldo -= retiro;
-          alert("Retiro exitoso. Nuevo saldo: $" + saldo);
+    switch (opcion) {
+        case 1:
+            let ingreso = parseFloat(prompt("¿Cuánto deseas ingresar?"));
+        if (!isNaN(ingreso) && ingreso > 0) {
+            saldo += ingreso;
+            alert("Ingreso exitoso. Nuevo saldo: $" + saldo);
         } else {
-          alert("Fondos insuficientes.");
+            alert("Cantidad inválida.");
         }
-      } else {
-        alert("Cantidad inválida.");
-      }
-      break;
+        break;
 
-    case 3:
-      alert("Saldo disponible: $" + saldo);
-      break;
+        case 2:
+            let retiro = parseFloat(prompt("¿Cuánto deseas retirar?"));
+        if (!isNaN(retiro) && retiro > 0) {
+            if (retiro <= saldo) {
+            saldo -= retiro;
+                alert("Retiro exitoso. Nuevo saldo: $" + saldo);
+            } else {
+                alert("Fondos insuficientes.");
+            }
+        } else {
+            alert("Cantidad inválida.");
+        }
+        break;
 
-    case 4:
-      alert("Gracias por usar tu cuenta bancaria.");
-      break;
+        case 3:
+        alert("Saldo disponible: $" + saldo);
+        break;
 
-    default:
-      alert("Opción no válida.");
-  }
-} while (opcion !== 4);
+        case 4:
+            alert("Gracias por usar tu cuenta bancaria.");
+        break;
+
+        default:
+            alert("Opción no válida.");
+    }
+    } while (opcion !== 4);
